adfc3b01b8a8095f9c74c6c0fcef7c2e49c18e9a
Ethan-Winer <ethan.m.winer@gmail.com>
2026-03-31 23:31:30 -0400

    should work on debian now

--- debian-install.sh
+++ debian-install.sh
@@ -4,6 +4,7 @@ sudo apt install ffmpeg -y
 sudo apt install pyenv -y
 pyenv install 3.13.2
 eval "$(pyenv init -)"
+pyenv local 3.13.2
 python -m pip install yt-dlp
 python -m pip install musicbrainzngs
 python -m pip install youtube-search-python
--- debian-run.sh
+++ debian-run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 eval "$(pyenv init -)"
+pyenv local 3.13.2
 python ./script.py
